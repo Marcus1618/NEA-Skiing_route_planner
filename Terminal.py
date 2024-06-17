ee1ef0c628e8c4b8a129003cc2854fd2974f3330
Marcus1618 <mjwentzell@perse.co.uk>
2024-06-17 14:42:54 +0100

    Adjacency list structuring

--- Terminal.py
+++ Terminal.py
@@ -1,6 +1,8 @@
 from Ui import Ui
+from Ski_resorts import ski_resorts #HOW AM I IMPORTING THIS
     
 class Terminal(Ui):
     
     def run(self):
-        pass
+        length = int(input("How long do you want to ski for (hours.mins): ")) #VALIDATE
+        print("Possible ski resorts: ")
